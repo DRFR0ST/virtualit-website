56e7d74183f098ff592e9b86f92c356623490bd5
DrFR0ST <mike.eling97@gmail.com>
2017-12-19 20:44:29 +0100

    📦 Implemented slick carousel

--- src/js/init.js
+++ src/js/init.js
@@ -10,8 +10,19 @@ $(document).ready(function() {
 	  edge: 'right', // Choose the horizontal origin
 	  closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
 	  draggable: true // Choose whether you can drag to open on touch screens,
-	}
-	);
+	});
+
+	$('.slick-carousel').slick({
+		autoplay: true,
+		centerMode: true,
+		draggable: true,
+		infinite: true,
+		slidesToShow: 3,
+  		slidesToScroll: 3,
+  		centerPadding: '60px',
+  		arrows: true,
+  		focusOnSelect: true,
+	});
 
 	var options = [
 		{selector: '#services-section', offset: 250, callback: function() {
